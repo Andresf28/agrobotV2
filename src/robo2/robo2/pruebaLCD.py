8b68419e3eccb31de63d1744b58b16debb928dbe
Jhon Edward <jhon_edw.gonzalez@uao.edu.co>
2024-05-21 22:34:54 -0500

    Adding LCD

---
+++ src/robo2/robo2/pruebaLCD.py
@@ -0,0 +1,22 @@
+import LiquidCrystal_I2C  
+from time import sleep  
+lcd=LiquidCrystal_I2C.lcd()  
+lcd.clear()  
+lcd.display("Testing....",1,0)  
+sleep(1)
+lcd.clear()
+for j in range(1,3):
+     for i in range(16):
+         lcd.display("*",j,i)
+         sleep(0.1)
+lcd.clear()   
+while True:  
+   try: 
+     lcd.display("Enter String you",1,0)
+     lcd.display("want to display",2,0) 
+     sleep(1)
+     lcd.display("Sebastian la mas perra",1,0)
+     sleep(2)
+     lcd.clear()    
+   except KeyboardInterrupt:    
+     break  
