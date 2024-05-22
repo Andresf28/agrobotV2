d0b7dc8766363ff74c5fda1cc9f767bf694bc360
Jhon Edward <jhon_edw.gonzalez@uao.edu.co>
2024-05-22 01:30:07 -0500

    Fixing GPIO

--- src/robo2/robo2/aspersion.py
+++ src/robo2/robo2/aspersion.py
@@ -2,7 +2,7 @@
 import rclpy
 from rclpy.node import Node
 import RPi.GPIO as GPIO
-
+import time
 
 class Aspersion(Node): # MODIFY NAME
     def __init__(self):
@@ -21,20 +21,21 @@ class Aspersion(Node): # MODIFY NAME
         GPIO.setup(self.bombaPin, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self.valvula1Pin, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self.valvula2Pin, GPIO.OUT, initial=GPIO.LOW)
-
+        self.curr_value = GPIO.HIGH
         self.aspersion_continua()
     
     def aspersion_continua(self):
         
 
-        while True:
-        
-
+        while True:      
+            time.sleep(1)
+            print("CHAANDA")
             # Toggle the output every second a
-            GPIO.output(self.bombaPin, GPIO.HIGH)
-            GPIO.output(self.valvula1Pin, GPIO.HIGH)
-            GPIO.output(self.valvula2Pin, GPIO.HIGH)
-        
+            GPIO.output(self.bombaPin, self.curr_value)
+            GPIO.output(self.valvula1Pin, self.curr_value)
+            GPIO.output(self.valvula2Pin, self.curr_value)
+            print("CHANDA1")
+            self.curr_value ^= GPIO.HIGH
         
 
 
